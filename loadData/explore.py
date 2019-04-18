466217c6a5c5f49ea35bad402f17e027afa8284b
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-04-18 14:54:23 -0700

    exploring still

--- loadData/explore.py
+++ loadData/explore.py
@@ -1,1 +1,8 @@
-import bs4 as bs
+from bs4 import BeautifulSoup
+import requests
+url = "http://schedules.calpoly.edu/all_location_curr.htm"
+r=requests.get(url)
+soup=BeautifulSoup(r.content,'html.parser')
+divisions = soup.find_all("div")
+for count,division in enumerate(divisions):
+    print("division number {} is {}".format(count, division))

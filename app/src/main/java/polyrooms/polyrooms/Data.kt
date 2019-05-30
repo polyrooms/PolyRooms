af5c234e781e2aa59e1a089eb27577aa5d0421e5
LucasYoung <ly.youngs@gmail.com>
2019-05-29 20:34:35 -0700

    stage androidx change

--- app/src/main/java/polyrooms/polyrooms/Data.kt
+++ app/src/main/java/polyrooms/polyrooms/Data.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.database.*
-import kotlin.jvm.internal.FunctionReference
 
 enum class Day {
     SUN, MON, TUE, WED, THU, FRI, SAT

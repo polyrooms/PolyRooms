be9be8c82af4580a345c55860d943f1ff24bec3a
Dhruv Singal <dhruvsingalabc@gmail.com>
2019-06-03 02:28:44 -0700

    removed duplicate and useless rows. Todo: parse ICS hrefs

--- loadData/explore.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-url = "http://schedules.calpoly.edu/all_location_curr.htm"
-r=requests.get(url)
-soup=BeautifulSoup(r.content,'html.parser')
-divisions = soup.find_all("div")
-for count,division in enumerate(divisions):
-    print("division number {} is {}".format(count, division))

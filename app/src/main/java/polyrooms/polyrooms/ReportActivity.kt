85d1c9b4eb4074cd02a0737289594501a81cb2ff
Nick <nbaggett@calpoly.edu>
2019-06-03 19:51:53 -0700

    Updated report activity

--- app/src/main/java/polyrooms/polyrooms/ReportActivity.kt
+++ app/src/main/java/polyrooms/polyrooms/ReportActivity.kt
@@ -1,6 +1,7 @@
 package polyrooms.polyrooms
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class ReportActivity : AppCompatActivity() {
@@ -8,5 +9,9 @@ class ReportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
+    }
+
+    fun onSubmit(view : View) {
+
     }
 }
